c96324931a7bca6343d628632d8b6d6688147ad8
kotobuki <64915142+kotobuki5991@users.noreply.github.com>
2023-09-03 16:58:46 +0900

    wip_stage5

--- cmd/mygit/main.go
+++ cmd/mygit/main.go
@@ -5,6 +5,7 @@ import (
 	"bytes"
 	"compress/zlib"
 	"crypto/sha1"
+	"encoding/hex"
 	"fmt"
 	"io"
 	"io/ioutil"
@@ -15,7 +16,7 @@ import (
 	"os"
 
 	"github.com/codecrafters-io/git-starter-go/cmd/mygit/util"
-	myzlib "github.com/codecrafters-io/git-starter-go/cmd/mygit/zlib"
+	// myzlib "github.com/codecrafters-io/git-starter-go/cmd/mygit/zlib"
 )
 
 const (
@@ -52,7 +53,23 @@ func main() {
 			lsTree(treeHash)
 		}
 	case "write-tree":
-		writeTree()
+		dirname := os.Args[len(os.Args)-1]
+		if len(os.Args) < 3 {
+			dirname = "."
+		}
+		objectname, err := writeTree(dirname, true)
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "write-tree: %v\n", err)
+			os.Exit(1)
+		}
+		fmt.Println(objectname)
+		// writeTree()
+	case "commit-tree":
+		treeSha := os.Args[2]
+		commitSha := os.Args[4]
+		commitMsg := os.Args[len(os.Args)-1]
+
+		commitTree(treeSha, commitSha, commitMsg)
 	default:
 		fmt.Fprintf(os.Stderr, "Unknown command %s\n", command)
 		os.Exit(1)
@@ -97,18 +114,20 @@ func catFile()  {
 func hashObject() {
 	fileName := os.Args[len(os.Args)-1]
 
-	data, err := ioutil.ReadFile(fileName)
-	if err != nil {
-		fmt.Fprintf(os.Stderr, "Error read file: %s\n", err)
+	blobData, err := util.GetBlobDataByFileName(fileName)
+	if err != nil && !os.IsExist(err) {
+		fmt.Fprintf(os.Stderr, "Failed get blobdata: %s\n", err)
+		return
 	}
-	header := fmt.Sprintf("blob %d\x00", len(data))
-	blobData := append([]byte(header), data...)
 
 	// SHA-1ハッシュの計算
-	sha := sha1.Sum(blobData)
-	objectId := fmt.Sprintf("%x", sha)
-	blobDire := string(objectId[:2])
-	blobFile := string(objectId[2:])
+	sha1Hex, err := util.GetHashByFileName(fileName)
+	if err != nil && !os.IsExist(err) {
+		fmt.Fprintf(os.Stderr, "Failed get hash: %s\n", err)
+		return
+	}
+	blobDire := string(sha1Hex[:2])
+	blobFile := string(sha1Hex[2:])
 	blobDirePath := filepath.Join(GIT_OBJECT_DIRE, blobDire)
 	blobFilePath := filepath.Join(blobDirePath, blobFile)
 
@@ -131,13 +150,45 @@ func hashObject() {
 	}
 	zw.Close()
 
-	fmt.Printf("%x", sha)
+	fmt.Printf(sha1Hex)
+}
+
+func HashObject(filepath string, write bool) (objectname string, err error) {
+	// read file once to both compute sha-1 sum and compress using zlib
+	// with larger files, using os.Open might be better
+	contents, err := ioutil.ReadFile(filepath)
+	if err != nil {
+		return "", err
+	}
+	// https://alblue.bandlem.com/2011/08/git-tip-of-week-objects.html
+	prefix := fmt.Sprintf("blob %d\x00", len(contents))
+	data := append([]byte(prefix), contents...)
+	hasher := sha1.New()
+	hasher.Write(data)
+	objectname = hex.EncodeToString(hasher.Sum(nil))
+	compressed := bytes.NewBuffer(make([]byte, 0))
+	zlibWriter := zlib.NewWriter(compressed)
+	if _, err := zlibWriter.Write(data); err != nil {
+		return "", err
+	}
+	if err := zlibWriter.Close(); err != nil {
+		return "", err
+	}
+
+	if write {
+		err := WriteObject(objectname, compressed.Bytes())
+		if err != nil {
+			return objectname, nil
+		}
+	}
+	return objectname, nil
 }
 
 func lsTree(treeSha string) {
 	treeInfos := getTreeInfos(treeSha)
 	for _, v := range treeInfos {
 		blobInfo := strings.Split(v, " ")
+		// fmt.Println(blobInfo)
 		if len(blobInfo) == 2 && blobInfo[0] != "tree" {
 			dirName := blobInfo[1]
 			fmt.Println(dirName)
@@ -145,15 +196,147 @@ func lsTree(treeSha string) {
 	}
 }
 
-func writeTree() {
-	direZlibBlob, err := myzlib.CompressDire("/tmp")
+// func writeTree() {
+// 	modeFile := 100644
+// 	modeDir := 40000
+// 	treeDatasBlob := []byte{}
+// 	treeSize := 0
+// 	stagedFilePaths, stagedDirePaths, err := util.GetDireFilePathsWalk()
+// 	if err != nil {
+// 		fmt.Fprintf(os.Stderr, "Error file paths walk: %s\n", err)
+// 		os.Exit(1)
+// 	}
+// 	for _, v := range stagedFilePaths {
+// 		// fmt.Println(v)
+// 		filePathsSplit := strings.Split(v, "/")
+// 		fileName := filePathsSplit[len(filePathsSplit)-2]
+// 		if strings.Contains(v, ".git") {
+// 			continue
+// 		}
+// 		fileHash, err := util.GetHashByFileName(v)
+// 		if err != nil {
+// 			fmt.Fprintf(os.Stderr, "Error get file hash: %s\n", err)
+// 			os.Exit(1)
+// 		}
+// 		treeDataStr := fmt.Sprintf("%d %s\u0000 %s", modeFile, fileName, fileHash)
+// 		// fmt.Println(treeDataStr)
+// 		treeDatasBlob = append(treeDatasBlob, []byte(treeDataStr)...)
+// 		treeDatasBlob = append(treeDatasBlob, '\n')
+// 		size, err := util.CalculateHeaderSize(treeDataStr)
+// 		if err != nil {
+// 			fmt.Fprintf(os.Stderr, "Error get header size: %s\n", err)
+// 			os.Exit(1)
+// 		}
+// 		treeSize += size
+// 	}
+// 	for _, v := range stagedDirePaths {
+// 		dirPathsSplit := strings.Split(v, "/")
+// 		dirName := dirPathsSplit[len(dirPathsSplit)-1]
+// 		if strings.Contains(v, ".git") {
+// 			continue
+// 		}
+// 		dirHash, err := util.CalculateDirectoryHash(v)
+// 		if err != nil {
+// 			fmt.Fprintf(os.Stderr, "Error get dire hash: %s\n", err)
+// 			os.Exit(1)
+// 		}
+// 		treeDataStr := fmt.Sprintf("%d %s\u0000 %s", modeDir, dirName, dirHash)
+// 		// fmt.Println(treeDataStr)
+// 		treeDatasBlob = append(treeDatasBlob, []byte(treeDataStr)...)
+// 		treeDatasBlob = append(treeDatasBlob, '\n')
+// 		size, err := util.CalculateHeaderSize(treeDataStr)
+// 		if err != nil {
+// 			fmt.Fprintf(os.Stderr, "Error get header size: %s\n", err)
+// 			os.Exit(1)
+// 		}
+// 		treeSize += size
+// 	}
+
+// 	treeHeader := fmt.Sprintf("tree %d\\0\n", treeSize)
+// 	treeHeaderByte := []byte(treeHeader)
+// 	treeDatasBlob = append(treeHeaderByte, treeDatasBlob...)
+
+// 	fmt.Println(util.GetHashByBlob(treeDatasBlob))
+
+// 	// direZlibBlob, err := myzlib.CompressDire("/tmp")
+// 	// if err != nil {
+// 	// 	fmt.Fprintf(os.Stderr, "Error compress dir: %s\n", err)
+// 	// 	os.Exit(1)
+// 	// }
+// 	// fmt.Println(util.GetHashByBlob(direZlibBlob))
+// 	hashObject()
+// }
+
+func WriteObject(objectname string, contents []byte) error {
+	dirname, filename := objectname[:2], objectname[2:]
+	err := os.MkdirAll(fmt.Sprintf(".git/objects/%s", dirname), 0755)
 	if err != nil {
-		fmt.Fprintf(os.Stderr, "Error compress dir: %s\n", err)
-		os.Exit(1)
+		return err
 	}
-	fmt.Println(util.GetHashByBlob(direZlibBlob))
-	// for _, v := range filePaths {
-	// 	fmt.Println(v)
-	// }
-	// hashObject()
+	err = os.WriteFile(fmt.Sprintf(".git/objects/%s/%s", dirname, filename), contents, 0644)
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+func writeTree(dir string, write bool) (objectname string, err error) {
+	files, err := ioutil.ReadDir(dir)
+	if err != nil {
+		return "", err
+	}
+	tree := bytes.NewBuffer(nil)
+	for _, file := range files {
+		path := filepath.Join(dir, file.Name())
+		if file.IsDir() {
+			if file.Name()[0] == '.' {
+				continue
+			}
+			objectname, err := writeTree(path, write)
+			if err != nil {
+				return "", nil
+			}
+			hexName, err := hex.DecodeString(objectname)
+			if err != nil {
+				return "", nil
+			}
+			fmt.Fprintf(tree, "40000 %s\x00%s", file.Name(), hexName)
+			continue
+		}
+		objectname, err := HashObject(path, false)
+		if err != nil {
+			return "", err
+		}
+		hexName, err := hex.DecodeString(objectname)
+		if err != nil {
+			return "", nil
+		}
+		fmt.Fprintf(tree, "100644 %s\x00%s", file.Name(), hexName)
+	}
+	prefix := fmt.Sprintf("tree %d\x00", tree.Len())
+	data := append([]byte(prefix), tree.Bytes()...)
+	hasher := sha1.New()
+	hasher.Write(data)
+	objectname = hex.EncodeToString(hasher.Sum(nil))
+	compressed := bytes.NewBuffer(make([]byte, 0))
+	zlibWriter := zlib.NewWriter(compressed)
+	if _, err := zlibWriter.Write(data); err != nil {
+		return "", err
+	}
+	if err := zlibWriter.Close(); err != nil {
+		return "", err
+	}
+	if write {
+		err := WriteObject(objectname, compressed.Bytes())
+		if err != nil {
+			return objectname, err
+		}
+	}
+	return objectname, nil
+}
+
+func commitTree(treeSha string, commitSha string, commitMsg string) {
+	fmt.Println(treeSha)
+	fmt.Println(commitSha)
+	fmt.Println(commitMsg)
 }
--- cmd/mygit/util/util.go
+++ cmd/mygit/util/util.go
@@ -3,20 +3,24 @@ package util
 import (
 	"crypto/sha1"
 	"fmt"
+	"io/ioutil"
 	"os"
 	"path/filepath"
+	"sort"
+	"strings"
 )
 
-func GetFilePathsWalk() ([]string, error) {
+func GetDireFilePathsWalk() ([]string, []string, error) {
 	// カレントディレクトリを取得
 	currentDir, err := os.Getwd()
 	if err != nil {
 		fmt.Printf("カレントディレクトリの取得中にエラーが発生しました: %v\n", err)
-		return nil, err
+		return nil, nil, err
 	}
 
 	// カレントディレクトリ以下のファイルパスを取得
 	filePaths := []string{}
+	direPath := []string{}
 	err = filepath.Walk(currentDir, func(path string, info os.FileInfo, err error) error {
 		if err != nil {
 			fmt.Printf("ファイルパスの取得中にエラーが発生しました: %v\n", err)
@@ -25,16 +29,105 @@ func GetFilePathsWalk() ([]string, error) {
 		if !info.IsDir() {
 			filePaths = append(filePaths, path)
 		}
+		direPath = append(direPath, path)
 		return nil
 	})
 	if err != nil {
 		fmt.Printf("ファイルパスの取得中にエラーが発生しました: %v\n", err)
-		return []string{}, err
+		return nil, nil, err
 	}
-	return filePaths, nil
+	return filePaths, direPath, nil
 }
 
-func GetHashByBlob(blob []byte) [20]byte {
+func GetHashByBlob(blob []byte) string {
 	sha := sha1.Sum(blob)
-	return sha
+	sha1Hex := fmt.Sprintf("%x", sha)
+	return sha1Hex
+}
+
+func GetHashByFileName(fileName string) (string, error) {
+	blobData, err := GetBlobDataByFileName(fileName)
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "Error get blob: %s\n", err)
+	}
+
+	// SHA-1ハッシュの計算
+	sha1Hex := GetHashByBlob(blobData)
+	return sha1Hex, nil
+}
+
+func GetSha1ByFileName(fileName string) ([20]byte, error) {
+	data, err := ioutil.ReadFile(fileName)
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "Error read file: %s\n", err)
+	}
+	header := fmt.Sprintf("blob %d\x00", len(data))
+	blobData := append([]byte(header), data...)
+	sha := sha1.Sum(blobData)
+	// SHA-1ハッシュの計算
+	return sha, nil
+}
+
+func GetBlobDataByFileName(fileName string)([]byte, error) {
+	data, err := ioutil.ReadFile(fileName)
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "Error read file: %s\n", err)
+		return nil, err
+	}
+	header := fmt.Sprintf("blob %d\x00", len(data))
+	blobData := append([]byte(header), data...)
+	return blobData, nil;
+}
+
+
+// ディレクトリのSHA-1ハッシュを計算する関数
+func CalculateDirectoryHash(path string) (string, error) {
+	// ディレクトリ内のすべてのファイルとディレクトリのパスを再帰的に取得
+	var filePaths []string
+	err := filepath.Walk(path, func(filePath string, info os.FileInfo, err error) error {
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "Error walk path: %s\n", err)
+			return err
+		}
+		filePaths = append(filePaths, filePath)
+		return nil
+	})
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "Error walk path2: %s\n", err)
+		return "", err
+	}
+
+	// ファイルパスをソート
+	sort.Strings(filePaths)
+
+	// ハッシュを計算
+	h := sha1.New()
+	for _, filePath := range filePaths {
+		// ファイルの内容を読み込む
+		fileData, err := ioutil.ReadFile(filePath)
+		if err != nil {
+			// fmt.Fprintf(os.Stderr, "Error walk path3: %s\n", err)
+			continue
+			// return "", err
+		}
+		// ファイルの内容をハッシュに追加
+		h.Write(fileData)
+	}
+	// ハッシュを16進数文字列に変換して返す
+	return fmt.Sprintf("%x", h.Sum(nil)), nil
+}
+
+// ヘッダー情報のバイト数を計算する関数
+func CalculateHeaderSize(entry string) (int, error) {
+	// エントリを空白で分割
+	parts := strings.Split(entry, " ")
+
+	// モード、エントリ名、null文字を合計
+	mode := parts[0]
+	entryName := parts[1]
+	nullCharacter := "\x00"
+	headerInfo := mode + " " + entryName + nullCharacter
+
+	// ヘッダー情報のバイト数を返す
+	return len(headerInfo), nil
 }
